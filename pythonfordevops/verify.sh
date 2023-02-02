654cdf08cd9f7cedb5077ce2514315042f454d37
ISierov <isier@softserveinc.com>
2023-02-02 11:05:16 +0200

    Update verify.sh

--- pythonfordevops/verify.sh
+++ pythonfordevops/verify.sh
@@ -1,2 +1,17 @@
-#!/bin/sh
-stat /output.json >/dev/null
+#!/bin/bash
+
+# Log for student file
+if [ -f $logsForStudent ];
+then rm -rf $logsForStudent;
+fi
+touch $logsForStudent
+
+
+# If correct you answer
+if diff --ignore-all-space correct_output.json output.json;
+then
+  echo "The files are identical." >> $logsForStudent
+else
+  echo "You have errors! The files are not identical." >> $logsForStudent
+  exit 1
+fi
