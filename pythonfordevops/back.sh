7255953de65ca02c49538b94336c346050a0bfb9
Ivan Sierov <isier@softserveinc.com>
2023-02-09 18:27:59 +0200

    Update back.sh

--- pythonfordevops/back.sh
+++ pythonfordevops/back.sh
@@ -7,6 +7,6 @@
 
 docker run -d -p 8080:8080 -v jenkins_home:/var/jenkins_home -e JAVA_OPTS="-Djenkins.install.runSetupWizard=false" --name jenkins jenkins/jenkins:lts
 
-crumb=$(curl -s -u admin:admin "https://1e97a604-8731-478d-8ef5-efbd5ae7b41f-10-244-28-34-8080.spch.r.killercoda.com/crumbIssuer/api/xml" | grep '<crumb>' | sed 's/<crumb>//g' | sed 's/<\/crumb>//g')
+crumb=$(curl -s -u admin:admin "https://{{TRAFFIC_HOST1_8080}}/crumbIssuer/api/xml" | grep '<crumb>' | sed 's/<crumb>//g' | sed 's/<\/crumb>//g')
 
-curl -u admin:admin -H "Jenkins-Crumb: $crumb" -X POST "https://1e97a604-8731-478d-8ef5-efbd5ae7b41f-10-244-28-34-8080.spch.r.killercoda.com/job/hello/build"
+curl -u admin:admin -H "Jenkins-Crumb: $crumb" -X POST "https://{{TRAFFIC_HOST1_8080}}/job/hello/build"
