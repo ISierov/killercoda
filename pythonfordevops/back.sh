8b78f9236b3a4b84ad5810df031d1f71942fefd7
Ivan Sierov <isier@softserveinc.com>
2023-02-09 12:16:50 +0200

    Update back.sh

--- pythonfordevops/back.sh
+++ pythonfordevops/back.sh
@@ -4,6 +4,8 @@ docker run -d -u root --rm --name jenkins -p 8080:8080 -p 50000:50000 --entrypoi
 
 docker exec -d jenkins bash -c 'git clone https://github.com/oveits/jenkins_home_alpine && export JENKINS_HOME=$(pwd)/jenkins_home_alpine && java -jar /usr/share/jenkins/jenkins.war 2>&1 1>/jenkins.log &'
 
+sleep 10
+
 # Create the "HelloWorldJob" job
 curl -X POST host01:8080/createItem?name=HelloWorldJob -H "Content-Type:application/xml" --data-binary @config.xml
 
