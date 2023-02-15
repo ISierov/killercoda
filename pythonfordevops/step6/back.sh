202b43c9a447370aa3ded7a85373880838145a8a
Ivan Sierov <isier@softserveinc.com>
2023-02-15 21:44:42 +0200

    new changes

--- pythonfordevops/step6/back.sh
+++ pythonfordevops/step6/back.sh
@@ -2,4 +2,4 @@
 
 docker run -d -u root --rm --name jenkins -p 8080:8080 -p 50000:50000 --entrypoint bash jenkins:2.46.2-alpine -c "tail -F /jenkins.log"
 
-docker exec -d jenkins bash -c 'git clone https://github.com/ISierov/jenkins_home_alpine && export JENKINS_HOME=$(pwd)/jenkins_home_alpine && java -jar /usr/share/jenkins/jenkins.war 2>&1 1>/jenkins.log &'
+docker exec -d jenkins bash -c 'git clone https://github.com/ISierov/jenkins_empty && export JENKINS_HOME=$(pwd)/jenkins_empty && java -jar /usr/share/jenkins/jenkins.war 2>&1 1>/jenkins.log &'
