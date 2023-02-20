82d235330a15abc341e6f553514a30583e5a7e19
Ivan Sierov <isier@softserveinc.com>
2023-02-20 11:37:20 +0200

    new changes

--- pythonfordevops/step6/back.sh
+++ pythonfordevops/step6/back.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-cd /root
-docker volume create jenkins_home
-git clone https://github.com/ISierov/jenkins_empty.git
-cd jenkins_empty
-docker build -t myjenkins .
-docker run -d -p 8080:8080 myjenkins
-sleep 20
-wget http://host1:8080/jnlpJars/jenkins-cli.jar
-java -jar jenkins-cli.jar -s http://host1:8080/ -webSocket build hello
+
+docker pull isierov/jenkins_builds:latest
+docker run --name jenkins -p 8080:8080 -d isierov/jenkins_builds:latest
